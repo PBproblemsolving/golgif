64b1043df813445e1d81743f8cd79718b434df7b
pbps <pbproblemsolving@protonmail.com>
2024-01-09 13:09:02 +0000

    function isolation from main

--- insert_goals_and_links.py
+++ insert_goals_and_links.py
@@ -12,61 +12,67 @@ logging.basicConfig(filename='insert_goals_and_links.log',
                      encoding='utf-8', level=logging.INFO)
 
 
+def insert_goals_and_links(con, db_submission):
+    try:
+        cur = con.cursor()
+        print(db_submission)
+        submission = reddit.submission(db_submission[0])
+        # side_links = links_from_comment() 
+        competitors = get_competitors(submission.title)
+        logging.info(submission.title)
+        try:
+            scorer = check_scorer_online(competitors[2])
+        except AttributeError as e:
+            logging.error('{} causes {}'.format(submission.title, e))
+        if scorer:
+            goal_data = goal_data_from_scorer(scorer, submission.created_utc)
+        else:
+            goal_data = None
+            logging.info('{} from {} causes No scorer'.format(competitors[2], submission.title))
+        if goal_data:
+            db_goal_data = check_data_prerequisites(con, goal_data)
+            goal_id = insert_goal(con, db_goal_data)
+            if goal_id:
+                result = add_goal_id_to_submission(cur, goal_id, db_submission[0])
+                if not result:
+                    con.commit()
+                else:
+                    con.rollback()
+                result = insert_link(cur, goal_id, submission.url, 1)
+                if not result:
+                    con.commit()
+                else:
+                    con.rollback()
+            else:
+                result = move_to_unidentified(cur, db_submission[0], db_submission[1], 1)
+                if not result:
+                    con.commit()
+                else:
+                    con.rollback()
+        else:  
+            result = move_to_unidentified(cur, db_submission[0], db_submission[1], 0)
+            if not result:
+                con.commit()
+            else:
+                con.rollback()
+    except sqlite3.Error as e:
+        con.rollback()
+        cur.close()
+        con.close()
+        logging.exception(e)
+
 if __name__ == '__main__':
     while True:
-        #sleep(3600)
         try:
             con = sqlite3.connect(DB)
             cur = con.cursor()
             get_new_submissions_6(cur)
-            result = cur.fetchall()
-            for db_submission in result:
-                print(db_submission)
-                submission = reddit.submission(db_submission[0])
-                # side_links = links_from_comment() 
-                competitors = get_competitors(submission.title)
-                logging.info(submission.title)
-                try:
-                    scorer = check_scorer_online(competitors[2])
-                except AttributeError as e:
-                    logging.error('{} causes {}'.format(submission.title, e))
-                    continue
-                if scorer:
-                    goal_data = goal_data_from_scorer(scorer, submission.created_utc)
-                else:
-                    goal_data = None
-                    logging.info('{} from {} causes No scorer'.format(competitors[2], submission.title))
-                if goal_data:
-                    db_goal_data = check_data_prerequisites(con, goal_data)
-                    goal_id = insert_goal(con, db_goal_data)
-                    if goal_id:
-                        result = add_goal_id_to_submission(cur, goal_id, db_submission[0])
-                        if not result:
-                            con.commit()
-                        else:
-                            con.rollback()
-                        result = insert_link(cur, goal_id, submission.url, 1)
-                        if not result:
-                            con.commit()
-                        else:
-                            con.rollback()
-                    else:
-                        result = move_to_unidentified(cur, db_submission[0], db_submission[1], 1)
-                        if not result:
-                            con.commit()
-                        else:
-                            con.rollback()
-                else:
-                    result = move_to_unidentified(cur, db_submission[0], db_submission[1], 0)
-                    if not result:
-                        con.commit()
-                    else:
-                        con.rollback()
-                
-        except sqlite3.Error as e:
+            result = cur.fetchone()
+            insert_goals_and_links(con, result)   
+        except Exception as e:
             con.rollback()
             cur.close()
             con.close()
             logging.exception(e)
             continue
-        sleep(3600)        
+        sleep(60)        
